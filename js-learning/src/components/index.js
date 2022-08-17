e1eb5a12d46a3ce9562956e28e20729b5876350e
Bayar OneZero <stereotronic7@gmail.com>
2022-08-17 17:10:57 +0800

    started hoobank-pj

---
+++ js-learning/src/components/index.js
@@ -0,0 +1,26 @@
+// Useful file for exporting necessary files to App.jsx
+//instead of importing all components into App.jsx one by one and making the code look longer
+
+import Navbar from './Navbar';
+import Billing from './Billing';
+import CardDeal from './CardDeal';
+import Business from './Business';
+import Clients from './Clients';
+import CTA from './CTA';
+import Stats from './Stats';
+import Footer from './Footer';
+import Testimonials from './Testimonials';
+import Hero from './Hero';
+
+export {
+    Navbar,
+    Business,
+    Billing,
+    CardDeal,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+};
