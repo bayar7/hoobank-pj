c45fb278537f8afe1cecc75fc8dd735a59999e07
Bayar OneZero <stereotronic7@gmail.com>
2022-08-18 21:37:05 +0800

    Hero section complete

--- js-learning/src/App.jsx
+++ js-learning/src/App.jsx
@@ -4,20 +4,20 @@ import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients
 
 
 const App = () => (
-  <div className='bg-slate-800 w-full overflow-hidden'>
+  <div className='bg-primary w-full overflow-hidden'>
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
 
-  <div className={`bg-slate-800 ${styles.flexStart}`}>
+  <div className={`bg-primary ${styles.flexStart}`}>
     <div className={`${styles.boxWidth}`}>
       <Hero />
     </div>
   </div>  
   
-  <div className={`bg-slate-800 ${styles.paddingX} ${styles.flexStart}`}>
+  <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
     <div className={`${styles.boxWidth}`}>
       <Stats />
       <Business />
