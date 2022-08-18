79d194e09ed22c1f36d49bee0ecfea71cb41cc79
Bayar OneZero <stereotronic7@gmail.com>
2022-08-18 23:11:19 +0800

    Added Get started button inside business component

--- js-learning/src/components/Business.jsx
+++ js-learning/src/components/Business.jsx
@@ -5,10 +5,18 @@ import Button from './Button';
 const Business = () => {
   return (
     <section id="features" className={layout.section}>
-      <div className={layout.sectionInfo}></div>
-        <h2 className={styles.heading2}>You do business,<br className="sm:block hidden"/> we'll handle the money.</h2>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>You do business,<br className="sm:block hidden"/>
+         we'll handle the money.
+        </h2>
         {/* max-w-[470px] makes the paragraph text into a easy-to-read text block */}
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can imporve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          With the right credit card, you can imporve your financial life by building credit,
+           earning rewards and saving money. But with hundreds of credit cards on the market.
+        </p>
+
+        <Button styles="mt-10" />
+      </div>
     </section>
   )
 }
