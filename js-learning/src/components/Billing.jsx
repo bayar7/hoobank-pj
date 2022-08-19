95ca1ac50ee054763791327a779e6051997b81c1
Bayar OneZero <stereotronic7@gmail.com>
2022-08-19 15:45:31 +0800

    Billing section complete

--- js-learning/src/components/Billing.jsx
+++ js-learning/src/components/Billing.jsx
@@ -9,6 +9,20 @@ const Billing = () => (
       <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"/>
     </div>
 
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Easily control your <br className="sm:block hidden" />
+       billing and invoicing. </h2>
+       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our next generation bank system uses cutting-edge 
+       technology stacks to provide seamless integration to all the available banking systems.
+       </p>
+
+       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+       <img src={apple} alt="apple_store" 
+             className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"/>
+        <img src={google} alt="google_store" 
+            className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"/>
+       </div>
+    </div>
   </section>
 )
 
