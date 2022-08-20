14f80e3a7fc349ac158490dcfe1bacfa84d2ea1c
Bayar OneZero <stereotronic7@gmail.com>
2022-08-20 12:47:20 +0800

    Footer complete

--- js-learning/src/components/Footer.jsx
+++ js-learning/src/components/Footer.jsx
@@ -32,7 +32,7 @@ const Footer = () => (
       </div>
     </div>
 
-    <div>
+    <div className="w-full  flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
         2022 HooBank. All Rights Reserved.
       </p>
@@ -42,7 +42,7 @@ const Footer = () => (
            <img key={social.id}
                 src={social.icon}
                 alt={social.id}
-                className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6'}`} />
+                className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6': 'mr-0'}`} />
         ))}
       </div>
     </div>
